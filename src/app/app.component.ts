abd1023b7acfedf14881d3061da7125b6994671d
Weksley Lima <weksley.lima@ucred.net.br>
2024-02-15 15:26:52 -0300

    Tela de login e tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuLoginComponent } from "./pages/menu-login/menu-login.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [MenuLoginComponent]
 })
 export class AppComponent {
   title = 'DoeGO';
